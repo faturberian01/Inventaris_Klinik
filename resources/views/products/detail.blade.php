8b6727b90d28ff4b484f70535db43dd6cd691d49
faturberian01 <faturbrian01@gmail.com>
2024-06-15 18:31:52 +0700

    Button X

--- resources/views/products/detail.blade.php
+++ resources/views/products/detail.blade.php
@@ -54,7 +54,7 @@
                                                                 <div class="modal-content">
                                                                     <div class="modal-header">
                                                                         <h5 class="modal-title" id="deleteModalLabel">Delete Stock</h5>
-                                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
                                                                     </div>
                                                                     <div class="modal-body">
                                                                         <form action="{{ route('products.stock', ['product' => $product, 'stock' => $stock]) }}" method="POST" id="deleteForm-{{ $stock->id }}">
